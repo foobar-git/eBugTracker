e17c27074859280166fbe1c443ed916d3026f69e
masterfoo <foobar.masterfoo@gmail.com>
2022-09-05 17:05:10 +0200

    88 v.

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,5 +11,9 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<Bug> Bugs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Project> Projects { get; set; }
     }
 }
